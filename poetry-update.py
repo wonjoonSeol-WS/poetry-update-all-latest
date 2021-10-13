1c5afda470464fa311b9ba8a2b0111a37ed57594
Wonjoon Seol <wonjoon.seol@deliveryhero.co.kr>
2021-10-13 23:07:25 +0900

    Add poetry-update for --dev

--- poetry-update.py
+++ poetry-update.py
@@ -1,20 +1,34 @@
 import subprocess
 
 
-def get_outdated_packages() -> str:
-    output = subprocess.run(
-        ["poetry", "show", "--outdated"],
-        capture_output=True
-    ).stdout.decode()
+def run_subprocess(command: list[str]) -> str:
+    return subprocess.run(command, capture_output=True).stdout.decode()
 
+
+def _get_package_name(command: list[str]) -> list[str]:
+    output = run_subprocess(command) 
     return [line.split()[0] for line in output.split("\n") if len(line.split()) > 0]
 
 
+def get_package_names():
+    poetry_show_outdated = ["poetry", "show", "--outdated"]
+    all_packages = _get_package_name(poetry_show_outdated)
+    no_dev_packages = _get_package_name(poetry_show_outdated + ["--no-dev"])
+    dev_packages = list(set(all_packages) - set(no_dev_packages))
+    return no_dev_packages, dev_packages
+
+
 def update_all_packages():
-    packages = get_outdated_packages()
-    for package in packages:
+    no_dev_packages, dev_packages = get_package_names()
+
+    for package in no_dev_packages:
         subprocess.run(
-            ["poetry", "add", f"{package}@latest"],
+                ["poetry", "add", f"{package}@latest"],
+        )
+
+    for package in dev_packages:
+        subprocess.run(
+                ["poetry", "add", "--dev", f"{package}@latest"]
         )
 
 
